6b4b3b6d5cca2605fe5c95896f69d77f7887a044
THitokuse <hasegawa.taichiro@gmail.com>
2019-06-23 21:07:31 +0900

    Scalaで素因数分解をする。

--- Distance.scala
+++ Distance.scala
@@ -1,7 +1,7 @@
 object Distance extends App {
-    val kilometersPerHours = 11.0
-    val minutes = 100.0
-    val distance = kilometersPerHours * minutes / 60.0
+  val kilometersPerHours = 11.0
+  val minutes = 100.0
+  val distance = kilometersPerHours * minutes / 60.0
 
-    println(s"走った距離は${distance}キロメートルです")
+  println(s"走った距離は${distance}キロメートルです")
 }
